761e02e72755b0ae966db26369e61cde789e8156
ybleeho <hu.li@6e-group.com>
2020-05-25 13:39:14 +0800

    minor fix

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -3,13 +3,13 @@ import {Context} from "koa"
 
 class UserController {
 
-    public static create = async (ctx: Context) => {
+    public static create = async (ctx: Context): Promise<void> => {
         ctx.body = await User.create({
             name: ctx.request.body.name
         })
     };
 
-    public static get = async (ctx: Context) => {
+    public static get = async (ctx: Context): Promise<void> => {
         ctx.body = await User.findById(ctx.params.id)
     }
 
