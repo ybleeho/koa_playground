c066f20b0e5e751704a51b21bf58c774c3f0f2ee
李虎 <lihu@MacBook-Pro.localdomain>
2020-06-08 19:36:51 +0800

    committed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,43 @@
 import app from './app';
 import Db from './database';
-import {apolloServer} from 'graphql'
+import {ApolloServer, gql } from 'apollo-server-koa';
+
+const typeDefs = gql`
+  type Query {
+    id: ID,
+    hello: String
+    age: Int
+    getUser(id: ID): User
+  }
+  type User {
+    id: ID, 
+    name: String
+    createdAt: String
+  }
+  type Mutation {
+    getUser(id: ID): User
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query: {
+        id: () => '5ede1cf69cab8b8c96b7cc8b',
+        hello: () => 'Hello world!',
+        age: () => 24,
+        getUser: (parent, args) => {
+            console.log({parent, args})
+            return {
+                "id": "5ede1cf69cab8b8c96b7cc8b",
+                "name": "lee ho",
+                "createdAt": "2020-06-08T11:11:50.407Z"
+            }
+        }
+    },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
 const port = process.env.PORT || 3000;
 
 Db.connect().then();
